313a38452c2619e315d7c9c41aba534b34605941
Antonin-lhp5 <totemdebois@gmail.com>
2021-02-09 15:06:34 +0100

    save

--- view/menu.php
+++ view/menu.php
@@ -1,17 +1,16 @@
-<div class="bg-gray-800 shadow-lg">
-    <div class="container mx-auto max-w-screen-lg">
-        <nav class="flex justify-between items-center sm:items-baseline pb-3 sm:pb-0 pt-3 px-5 lg:px-0">
-            <div class="text-light tracking-wide space-x-4 hidden sm:inline-flex">
-                <a href="index.php" class="border-b-2 border-gray-500 pb-3 hover:text-white">Accueil</a>
-                <a href="explorer.php" class="border-b-2 border-pink-500 pb-3 hover:text-white">Explorer</a>
+<div class="bg-gray-800 shadow-lg h-16 flex items-center">
+    <div class="container mx-auto max-w-screen-xl">
+        <nav class="flex justify-between items-center pb-3 sm:pb-0 px-4 lg:px-0">
+            <div class="text-light tracking-wide space-x-4 hidden sm:flex">
+                <a href="index.php" class="hover:text-white border-b-2 border-gray-400">Accueil</a>
+                <a href="explorer.php" class="border-b-2 border-pink-500 hover:text-white">Explorer</a>
                 <a href="index.php#category" class="hover:text-white">Catégories</a>
                 <a href="index.php#news" class="hover:text-white">Nouveautés</a>
             </div>
             <h1 class="tracking-wide text-2xl font-bold sm:text-2xl font-title">Blabla<span class="text-indigo-400">quiz</span></h1>
-
             <!-- hamburger -->
-            <button class="sm:hidden outline-none focus:outline-none">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <button class="sm:hidden">
+                <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
                 </svg>
             </button>
