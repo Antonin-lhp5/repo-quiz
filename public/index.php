9d594cac29782bfcf9a51d147417dcd98487a078
Antonin-lhp5 <totemdebois@gmail.com>
2021-02-15 10:32:26 +0100

    changement view footer

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 require_once '../datas/text-data.php';
 require_once '../view/header.php';
 require_once '../view/index.html.php';
+require_once '../view/info.html.php';
 require_once '../view/footer.php';
 ?>
 
