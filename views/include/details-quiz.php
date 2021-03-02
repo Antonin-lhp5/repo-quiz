3bae3a936f31aad9aa6722f1a8484d6db0caf93d
Antonin-lhp5 <totemdebois@gmail.com>
2021-03-02 16:53:55 +0100

    ajout détail des quiz ok

---
+++ views/include/details-quiz.php
@@ -0,0 +1,28 @@
+<div class="bg-gray-700 p-5 ring-black ring-2 ring-opacity-10 shadow">
+    <div>
+        <label class="block mb-2 text-gray-200" for="titleQuiz">Nom du quiz</label>
+        <input class="w-full p-2 mb-6 text-gray-400 bg-gray-800 focus:border-transparent" type="text" name="titleQuiz" id="titleQuiz" value="<?= $detailsQuizArray['qTitle'] ?>" disabled>
+    </div>
+    <div>
+        <label class="block text-gray-200 mb-2" for="imgQuiz">Image du quiz</label>
+        <img class="h-20 w-20 object-cover md:h-24 md:w-24 flex-shrink-0 rounded-lg mb-2" src="assets/upload<?= $detailsQuizArray['qImg'] ?>" alt="">
+        <input class="mb-6" type="file" id="myFile" name="imgQuiz" disabled>
+    </div>
+    <div>
+        <label class="block mb-2 text-gray-200" for="categoryQuiz">Catégorie du quiz</label>
+        <select class="bg-gray-800" name="categoryQuiz" id="categoryQuiz" disabled>
+            <option selected disabled>Choix de la catégorie</option>
+            <?php
+            foreach ($allCategoryArray as $category) { ?>
+                <option value="<?= $category['id_category'] ?>" <?= $category['id_category'] == $detailsQuizArray['id_category'] ? 'selected' : '' ?>><?= $category['categorie'] ?></option>
+            <?php
+            } ?>
+        </select>
+
+        <form action="modifyQuiz.php" method="POST">
+            <button type="submit" class="mt-5 bg-gray-600 hover:bg-gray-700 text-gray-200 font-bold py-2 px-4 rounded outline-none" name="modifyQuiz" value="<?= $detailsQuizArray['idQuiz'] ?>">Modifier</button>
+            <a type="button" href="library.php" class="mt-5 bg-gray-600 hover:bg-gray-700 text-gray-200 font-bold py-2 px-4 rounded outline-none">Retour</a>
+        </form>
+
+    </div>
+</div>
---
+++ views/view-detailsQuiz.php
@@ -0,0 +1,21 @@
+<div class="container mx-auto max-w-screen-md md:py-10">
+  <div class="flex flex-col w-full text-gray-200 mt-5 md:mt-0">
+    <div class="text-xl pb-3 px-2">Module de création de quiz</div>
+    <div class="pb-3 px-2">Modifier le quiz</div>
+
+    <?php
+    // Nous testons si nous arrivons à obtenir les infos du quiz sous forme d'un tableau
+    if ($detailsQuizArray) {
+      include 'include/details-quiz.php';
+      // si KO, nous indiquons à l'utilisateur que le quiz n'est pas présent
+    } else { ?>
+      <p class="text-lg text-red-500 text-center py-3"></i>Quiz non présent</p>
+      <div class="text-center">
+        <a type="button" href="detailsQuiz.php" class="mt-5 bg-blue-600 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded outline-none">Retour</a>
+      </div>
+    <?php
+    } ?>
+
+  </div>
+</div>
+</div>
