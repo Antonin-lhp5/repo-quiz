168cacd1f376b506bc99c9915ccd7195a31a8eec
Antonin-lhp5 <totemdebois@gmail.com>
2021-03-18 09:33:11 +0100

    suppr link useless + ok

--- views/include/form-modifyQuestion.php
+++ views/include/form-modifyQuestion.php
@@ -24,13 +24,11 @@
                 </button>
                 <a href="addQuestion.php?idQuiz=<?= $_SESSION['idQuiz'] ?>" class="bg-gray-800 hover:bg-gray-900 text-gray-200 font-bold py-2 px-4 rounded">Retour</a>
             </div>
-            <form action="">
-                <button type="button" class="bg-red-700 hover:bg-red-600 text-white text-sm px-2 py-2 show-modal">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5 text-gray-200">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                    </svg>
-                </button>
-            </form>
+            <button type="button" class="bg-red-700 hover:bg-red-600 text-white text-sm px-2 py-2 show-modal">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5 text-gray-200">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+            </button>
         </div>
     </form>
 </div>
@@ -49,13 +47,13 @@
         </div>
         <!-- modal body -->
         <div id="nameQuiz" class="p-3">
-            <p> Voulez-vous vraiment supprimer cette question ? Attention, ceci est irréversible !</p>
+            <p> Voulez-vous vraiment supprimer cette question ? Attention, cette action est irréversible !</p>
         </div>
         <div class="flex justify-end items-center w-100 border-t p-3">
             <form action="" method="POST">
-                <button id="deleteBtnModal" href="addQuestion.php?idQuiz=<?= $_SESSION['idQuiz'] ?>" name="deleteBtn" type="submit" value="<?= $questionInfoArray['id_question'] ?>" class="bg-red-600 hover:bg-red-700 px-3 py-1 rounded text-white mr-2">Supprimer</button>
+                <button id="deleteBtnModal" name="deleteBtn" type="submit" value="<?= $questionInfoArray['id_question'] ?>" class="bg-red-600 hover:bg-red-700 px-3 py-1 rounded text-white mr-2">Supprimer</button>
             </form>
-          
+
             <button class="bg-gray-600 hover:bg-gray-700 px-3 py-1 rounded text-white close-modal">Retour</button>
         </div>
     </div>
